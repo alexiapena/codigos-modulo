24a5349bccce6c53938fe2179839741f20135021
alexiapena <117307347+alexiapena@users.noreply.github.com>
2022-11-02 12:02:42 -0600

    Add files via upload

---
+++ yt_colores/eliminar.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once 'conexion.php';
+
+$id = $_GET['id'];
+
+$sql_eliminar = 'DELETE FROM COLORES WHERE id=?';
+$sentencia_eliminar = $pdo->prepare($sql_eliminar);
+$sentencia_eliminar->execute(array($id));
+
+
+$pdo = null;
+$sentencia_eliminar = null;
+
+
+header('location:index.php');
